29ae4b2b3e17428c123a90d8a5063651f9008cb1
Evandro Gubert <gubertevandro33@gmail.com>
2023-08-10 08:53:04 -0300

    Desafio Repositorios

---
+++ src/domain/repository/customer-repository.interface.ts
@@ -0,0 +1,7 @@
+import Customer from "../entity/customer";
+import RepositoryInterface from "./repository-interface";
+
+export default interface CustomertRepositoryInterface extends RepositoryInterface<Customer> {
+
+
+}
---
+++ src/domain/repository/order-repository.interface.ts
@@ -0,0 +1,9 @@
+import Order from "../entity/order";
+import Product from "../entity/product";
+import RepositoryInterface from "./repository-interface";
+
+export default interface OrderRepositoryInterface extends RepositoryInterface<Order> {
+
+    
+
+}
