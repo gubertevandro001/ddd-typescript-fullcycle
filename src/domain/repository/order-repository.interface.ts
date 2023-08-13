38c650f9e8705960db79325d63184d24e8c3fbaa
Evandro Gubert <gubertevandro33@gmail.com>
2023-08-13 19:21:42 -0300

    Update e organizaçao de pastas do projeto

--- src/domain/repository/order-repository.interface.ts
+++
@@ -1,9 +0,0 @@
-import Order from "../entity/order";
-import Product from "../entity/product";
-import RepositoryInterface from "./repository-interface";
-
-export default interface OrderRepositoryInterface extends RepositoryInterface<Order> {
-
-    
-
-}
